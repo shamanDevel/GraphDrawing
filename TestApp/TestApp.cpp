1ff1eb33af0cdb4798cfe2ae3c83fc0155da41b4
shamanDevel <muell.filter@gmail.com>
2015-04-21 13:56:56 +0200

    added sub-testprogram for scanning and indexing the rome graph library

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -2,11 +2,14 @@
 //
 
 #include "stdafx.h"
+
 #include <cstdlib>
 #include <sstream>
 #include <iostream>
 #include <set>
 #include <math.h>
+#include <string>
+#include <cstdio>
 #include <GraphGenerator.h>
 #include <GraphConverter.h>
 #include <CrossingMinimization.h>
@@ -14,8 +17,10 @@
 #include <OOCMCrossingMinimization.h>
 #include <boost/graph/connected_components.hpp>
 #include <boost/graph/boyer_myrvold_planar_test.hpp>
-#include "boost/iostreams/stream.hpp"
-#include "boost/iostreams/device/null.hpp"
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/null.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/graph/graphml.hpp>
 #include <ogdf_include.h>
 #include <ogdf\planarlayout\PlanarDrawLayout.h>
 #include <ogdf\energybased\FMMMLayout.h>
@@ -56,10 +61,62 @@ void TestMinimization() {
 	}
 }
 
+struct RomeGraphDescr
+{
+	string fileName;
+	int n;
+};
+
+void scanRomeGraphs(string folder, vector<RomeGraphDescr>& target)
+{
+	//TODO
+}
+
+void createRomeGraphsInfo(string folder)
+{
+	using namespace boost::filesystem;
+	path p (folder);   // p reads clearer than argv[1] in the following code
+	FILE* file;
+	fopen_s(&file, (folder + "Graphs.dat").c_str(), "w");
+	try
+	{
+		assert (exists(p));
+		assert (is_directory(p));
+		for (directory_iterator it = directory_iterator(p); it != directory_iterator(); ++it)
+		{
+			path f = it->path();
+			if (f.extension() == ".graphml") {
+				//load graph
+				Graph G;
+				ifstream is (f.c_str());
+				boost::dynamic_properties properties;
+				boost::read_graphml(is, G, properties);
+				cout << "Graph " << f.filename() << " loaded";
+				int n = num_vertices(G);
+				int m = num_edges(G);
+				bool planar = boost::boyer_myrvold_planarity_test(G);
+				cout << " nodes=" << n << " edges=" << m << " planar=" << planar << endl;
+				if (planar) {
+					fprintf(file, "%d %d %s\n", n, m, f.filename().string().c_str());
+				}
+			}
+		}
+	}
+	catch (const filesystem_error& ex)
+	{
+		cerr << ex.what() << '\n';
+	}
+	catch (const boost::graph_exception& ex)
+	{
+		cerr << ex.what() << '\n';
+	}
+	fclose(file);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	TestMinimization();
+	//TestMinimization();
+	createRomeGraphsInfo("C:\\Users\\Sebastian\\Documents\\C++\\GraphDrawing\\example-data\\");
 	cin.get();
 	return 0;
 }
